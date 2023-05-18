9d61391ec43a9990079554dbe28e091f602e8936
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-05-18 14:52:38 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "hereafter-pets-40559",
-  url: "https://hereafter-pets-40559.botics.co",
-  api: "https://hereafter-pets-40559.botics.co/api/v1"
+  "name": "hereafter-pets-40559",
+  "url": "https://hereafter-pets-40559.botics.co",
+  "api": "https://hereafter-pets-40559.botics.co/api/v1",
+  "initialRoute": "ScreenAI2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
