b3afe6d6265ad6ae96064a3e4ff3f923b4835d67
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-05-18 16:00:41 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI11/index.js
+++ screens/ScreenAI11/index.js
@@ -1,27 +1,70 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-const UserProfileScreen = () => {
-  const [name, setName] = useState('');
-  const [breed, setBreed] = useState('');
-  const [petType, setPetType] = useState('');
+const HomeScreen = () => {
   return <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image source={{
-        uri: 'https://tinyurl.com/42evm3m3'
-      }} style={styles.image} />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Image source={{
+          uri: 'https://tinyurl.com/42evm3m3'
+        }} style={styles.profileImage} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image source={{
+          uri: 'https://tinyurl.com/42evm3m3'
+        }} style={styles.petImage} />
+        </TouchableOpacity>
       </View>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Name" value={name} onChangeText={setName} />
-        <TextInput style={styles.input} placeholder="Breed" value={breed} onChangeText={setBreed} />
-        <View style={styles.dropdownContainer}>
-          <Text style={styles.dropdownLabel}>Type of Pet:</Text>
-          <TouchableOpacity style={styles.dropdownButton} onPress={() => console.log('Dropdown pressed')}>
-            <Text style={styles.dropdownButtonText}>{petType || 'Select'}</Text>
+      <View style={styles.services}>
+        <Text style={styles.servicesTitle}>Services Available</Text>
+        <View style={styles.servicesList}>
+          <TouchableOpacity style={styles.service}>
+            <Image source={{
+            uri: 'https://tinyurl.com/42evm3m3'
+          }} style={styles.serviceImage} />
+            <Text style={styles.serviceTitle}>Service 1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.service}>
+            <Image source={{
+            uri: 'https://tinyurl.com/42evm3m3'
+          }} style={styles.serviceImage} />
+            <Text style={styles.serviceTitle}>Service 2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.service}>
+            <Image source={{
+            uri: 'https://tinyurl.com/42evm3m3'
+          }} style={styles.serviceImage} />
+            <Text style={styles.serviceTitle}>Service 3</Text>
           </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.saveButton} onPress={() => console.log('Save pressed')}>
-          <Text style={styles.saveButtonText}>Save</Text>
+        <TouchableOpacity style={styles.catalogButton}>
+          <Text style={styles.catalogButtonText}>Catalog</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.sideMenu}>
+        <TouchableOpacity style={styles.sideMenuItem}>
+          <Text style={styles.sideMenuItemText}>Edit Profile</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.sideMenuItem}>
+          <Text style={styles.sideMenuItemText}>Home Screen</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.sideMenuItem}>
+          <Text style={styles.sideMenuItemText}>Schedule Service</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.sideMenuItem}>
+          <Text style={styles.sideMenuItemText}>Product List</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.sideMenuItem}>
+          <Text style={styles.sideMenuItemText}>Memorial Wall</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.sideMenuItem}>
+          <Text style={styles.sideMenuItemText}>Settings</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.sideMenuItem}>
+          <Text style={styles.sideMenuItemText}>Send Feedback</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.sideMenuItem}>
+          <Text style={styles.sideMenuItemText}>Sign Out</Text>
         </TouchableOpacity>
       </View>
     </View>;
@@ -31,59 +74,85 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingTop: 50
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    marginBottom: 30
+  },
+  profileImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25
+  },
+  petImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25
+  },
+  services: {
+    flex: 1,
     justifyContent: 'center'
   },
-  imageContainer: {
-    width: '100%',
-    height: 200,
-    alignItems: 'center',
-    justifyContent: 'center',
+  servicesTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
     marginBottom: 20
   },
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover'
-  },
-  inputContainer: {
-    width: '80%'
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10
-  },
-  dropdownContainer: {
+  servicesList: {
     flexDirection: 'row',
-    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20
+  },
+  service: {
+    alignItems: 'center'
+  },
+  serviceImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
     marginBottom: 10
   },
-  dropdownLabel: {
-    marginRight: 10
+  serviceTitle: {
+    fontSize: 16,
+    fontWeight: 'bold'
   },
-  dropdownButton: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    flex: 1
+  catalogButton: {
+    backgroundColor: '#000',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    alignSelf: 'center'
   },
-  dropdownButtonText: {
-    color: '#555'
-  },
-  saveButton: {
-    backgroundColor: '#007AFF',
-    borderRadius: 5,
-    padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  saveButtonText: {
+  catalogButtonText: {
     color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  sideMenu: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    width: '70%',
+    backgroundColor: '#fff',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    justifyContent: 'space-between'
+  },
+  sideMenuItem: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    width: 641,
+    height: 60
+  },
+  sideMenuItemText: {
+    fontSize: 18,
     fontWeight: 'bold'
   }
 });
-export default UserProfileScreen;
+export default HomeScreen;
